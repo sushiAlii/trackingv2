91d051e10988791add51add756117cf8b13bb5a4
sushiAlii <aliasgar.laut@gmail.com>
2021-05-05 03:02:18 +0800

    making comments on API routes

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//=====================AUTH========================
 Route::post(uri: '/login', action: [App\Http\Controllers\Api\Auth\AuthController::class, 'login']);
 Route::post(uri: '/logout', action: [App\Http\Controllers\Api\Auth\AuthController::class, 'logout'])->middleware('auth:sanctum');
+//=====================AUTH========================
 
-Route::get(uri: 'documents', action: [App\Http\Controllers\Api\DocumentController::class, 'ppmp']);
+Route::get(uri: '/manage_users', action: [App\Http\Controllers\Api\UserController::class, 'index']);
+
+Route::get(uri: '/offices',action: [App\Http\Controllers\Api\OfficeController::class, 'index']);
+
+Route::get(uri: 'documents', action: [App\Http\Controllers\Api\PPMPController::class, 'index']);
 Route::get(uri: 'documents/tracking/id={id}', action: [App\Http\Controllers\Api\DocumentDetailController::class, 'showPPMPTravels']);
 
 Route::get(uri: 'documents/details/id={id}', action: [App\Http\Controllers\Api\DocumentDetailController::class, 'showPPMPDetails']);
 Route::get(uri: 'documents/details/ppmp={id}', action: [App\Http\Controllers\Api\DocumentController::class, 'ppmpWithId']);
+
+Route::post(uri: '/scan', action: [App\Http\Controllers\Api\PPMPTravelLogController::class, 'store']);
