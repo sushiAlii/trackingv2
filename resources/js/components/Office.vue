fd405f5da0a4525984912d45bf3e01f9ef612a4d
sushiAlii <aliasgar.laut@gmail.com>
2021-05-05 03:14:40 +0800

    creating office view

---
+++ resources/js/components/Office.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app>
+        <Navbar/>
+        <v-main class="mx-4 my-5">
+            <h1>Offices</h1>
+        </v-main>
+    
+        
+    </v-app>
+</template>
+
+<script>
+    import Navbar from './Navbar'
+
+    export default {
+        components: {
+            Navbar
+        },
+        data(){
+            return {
+                search: '',
+                loadingvariable: true,
+                users: [],
+                headers: [
+                    {
+                        text: 'User',
+                        align: 'start',
+                        value: 'name'
+                    },
+                    { text: 'Email', value: 'email' },
+                    { text: 'Role', value: 'role' },
+                    { text: 'Office', value: 'office' }
+                ]
+            }
+        },
+        mounted(){
+            this.loadOffices();
+        },
+        methods: {
+            loadOffices: function(){
+
+            }
+        }
+    }
+</script>
